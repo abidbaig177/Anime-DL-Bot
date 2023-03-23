3c1fc74353069094ae5f18e3e6cced0e88fddfd7
abidbaig177 <119119875+abidbaig177@users.noreply.github.com>
2023-03-23 21:44:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = ""
-api_hash = ""
-bot_token = ""
+api_id = "20124301"
+api_hash = "8f39dd0f8b439da5e64c1fab71991a6b"
+bot_token = "6013387746:AAERqoJjTWCpwrk2TY-szM4itBS-ajOqewU"
